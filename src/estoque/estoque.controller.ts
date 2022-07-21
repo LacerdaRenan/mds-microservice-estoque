c97d0e2e6af596e78ee8f593df4ca5d9bfe88695
LacerdaRenan <renanrodlacerda@gmail.com>
2022-07-20 21:31:22 -0300

    iniciando projeto

---
+++ src/estoque/estoque.controller.ts
@@ -0,0 +1,33 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { EstoqueService } from './estoque.service';
+import { CreateEstoqueDto } from './dto/create-estoque.dto';
+
+@Controller('estoque')
+export class EstoqueController {
+  constructor(private readonly estoqueService: EstoqueService) {}
+
+  @Post()
+  create(@Body() createEstoqueDto: CreateEstoqueDto) {
+    return this.estoqueService.create(createEstoqueDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.estoqueService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.estoqueService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateEstoqueDto: CreateEstoqueDto) {
+    return this.estoqueService.update(+id, updateEstoqueDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.estoqueService.remove(+id);
+  }
+}
